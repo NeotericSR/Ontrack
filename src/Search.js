f4bfe4b161456bd80606e1d1e434687ecc156ba6
Okoye Bilikha <neotericsr@gmail.com>
2020-06-30 22:57:39 +0100

    ...

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Search(props) {
     return (
-        <input className="App__search" type="search" value={props.value} onChange={props.updateSearch.bind(this)}/>
+        <input className="App__search" type="search" value={props.value} onChange={props.updateSearch.bind(this)} onKeyPress={props.addToFilter.bind(this)}/>
     )
 }
